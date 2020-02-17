155f4f3208d478354bd45913de2b7d2c4f69e862
Andrei Nitescu <nitescua@yahoo.com>
2020-02-17 15:38:00 +0200

    #9 [Code Generation] Wrong literals for glyphs from upper bit planes

--- js/utils/stringUtils.js
+++ js/utils/stringUtils.js
@@ -1,11 +1,14 @@
 export function toUnicodeString(unicode) {
     let unicodeString = unicode.toString(16);
 
+    let prefix;
     if(unicodeString.length <= 4) {
         unicodeString = unicodeString.padStart(4, "0");
+        prefix = "u";
     } else {
         unicodeString = unicodeString.padStart(8, "0");
+        prefix = "U";
     }
 
-    return `\\u${unicodeString}`;
+    return `\\${prefix}${unicodeString}`;
 }
